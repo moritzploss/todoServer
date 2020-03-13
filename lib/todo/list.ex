645cf77f93b9da8f0ac244538152626888c29dc1
Moritz Ploss <moritz.ploss@relevant-digital.com>
2020-03-13 17:21:49 +0100

    refactoring list update_entry function

--- lib/todo/list.ex
+++ lib/todo/list.ex
@@ -22,11 +22,8 @@ defmodule Todo.List do
   end
 
   def update_entry(%List{} = todo_list, entry_id, %Entry{} = entry) when is_id(entry_id) do
-    with {:ok, _entry} <- Map.fetch(todo_list.entries, entry_id),
-      new_list <- Map.put(todo_list, entry_id, entry)
-    do
-      {:ok, new_list}
-    else
+    case Map.fetch(todo_list.entries, entry_id) do
+      {:ok, _entry} -> {:ok, Map.put(todo_list, entry_id, entry)}
       :error -> {:error, :no_entry_with_id}
     end
   end
