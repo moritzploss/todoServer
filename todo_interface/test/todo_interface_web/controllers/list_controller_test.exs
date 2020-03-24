eb5c3b450d1fec813015120b3b36f896ccd99671
Moritz Ploss <moritz.ploss@relevant-digital.com>
2020-03-24 20:21:00 +0100

    clean-up server state after each ListController test and expand controller test suite

--- todo_interface/test/todo_interface_web/controllers/list_controller_test.exs
+++ todo_interface/test/todo_interface_web/controllers/list_controller_test.exs
@@ -1,33 +1,71 @@
 defmodule TodoInterfaceWeb.ListControllerTest do
   use TodoInterfaceWeb.ConnCase
 
-  setup do
+  alias Todo.UserManager
+
+  setup_all do
     %{user_id: "123"}
   end
 
+  setup %{user_id: user_id} do
+    on_exit(fn -> UserManager.stop_list_manager(user_id) end)
+  end
+
   test "index/3 responds with empty list if user has no lists", %{conn: conn, user_id: user_id} do
-    response =
-      conn
-      |> get(Routes.user_list_path(conn, :index, user_id))
-      |> json_response(200)
+    response = conn
+    |> get(Routes.user_list_path(conn, :index, user_id))
+    |> json_response(200)
 
     assert response == %{"lists" => []}
   end
 
   test "create/3 responds with new list", %{conn: conn, user_id: user_id} do
-    response =
-      conn
-      |> post(Routes.user_list_path(conn, :create, user_id))
-      |> json_response(200)
+    response = conn
+    |> post(Routes.user_list_path(conn, :create, user_id))
+    |> json_response(200)
 
     assert %{
       "entries" => %{} = entries,
       "owner_id" => ^user_id,
       "id" => id,
     } = response
+  end
 
-    assert %{} = conn
-    |> delete(Routes.user_list_path(conn, :delete, user_id, id))
+  test "show/4 responds with list by id", %{conn: conn, user_id: user_id} do
+    newly_created_list = conn
+    |> post(Routes.user_list_path(conn, :create, user_id))
     |> json_response(200)
+
+    list_via_get = conn
+    |> get(Routes.user_list_path(conn, :show, user_id, newly_created_list["id"]))
+    |> json_response(200)
+
+    assert newly_created_list === list_via_get
+  end
+
+  test "delete/4 deletes list by id", %{conn: conn, user_id: user_id} do
+    includes_list_id? = fn %{"lists" => lists}, list_id ->
+      list_id in Enum.map(lists, fn %{"id" => id} -> id end)
+    end
+
+    %{"id" => list_id} = conn
+    |> post(Routes.user_list_path(conn, :create, user_id))
+    |> json_response(200)
+
+    conn
+    |> get(Routes.user_list_path(conn, :index, user_id))
+    |> json_response(200)
+    |> includes_list_id?.(list_id)
+    |> assert
+
+    %{} = conn
+    |> delete(Routes.user_list_path(conn, :delete, user_id, list_id))
+    |> json_response(200)
+
+    conn
+    |> get(Routes.user_list_path(conn, :index, user_id))
+    |> json_response(200)
+    |> includes_list_id?.(list_id)
+    |> refute
   end
 end
